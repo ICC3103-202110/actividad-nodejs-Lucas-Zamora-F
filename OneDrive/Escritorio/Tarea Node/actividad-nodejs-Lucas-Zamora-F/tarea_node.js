f110391350b91b7b73192c0afe4fefeebf64a7d8
Lucas-Zamora-F <Lzamora@miuandes.cl>
2021-04-27 13:51:24 -0400

    Tarea V1

--- OneDrive/Escritorio/Tarea Node/actividad-nodejs-Lucas-Zamora-F/tarea_node.js
+++ OneDrive/Escritorio/Tarea Node/actividad-nodejs-Lucas-Zamora-F/tarea_node.js
@@ -1,1 +1,18 @@
-.
+function fibonacci(x) {
+    if (x <= 1)
+        return x
+    else 
+        return fibonacci(x - 1) + fibonacci(x -2)
+        
+    
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+readline.question(`Select a Number: `, number => {
+  console.log(fibonacci(number))
+  readline.close()
+})
